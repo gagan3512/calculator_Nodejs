7283995aa65d0e4028982885877fc729f99cb310
gagan3512 <gagan1825.cse18@chitkara.edu.in>
2021-04-25 15:47:54 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,27 @@
+//jshint exversion:6
+
+const express = new require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/", function(req,res){
+   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/" , function(req,res){
+     
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1+num2;
+
+    res.send("The result of calculation is : " + result);
+
+
+});
+
+app.listen(3000 , function(){
+   console.log("server started at port : 3000");
+});
